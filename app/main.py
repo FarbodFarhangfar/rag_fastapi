d15b25d6879d13a8938ff877c7674365ee1b108e
FarbodFarhangfar <farbod.farhangfar.dev@gmail.com>
2024-02-26 15:25:04 +0330

    fixing pdf style

--- app/main.py
+++ app/main.py
@@ -14,9 +14,9 @@ def index():
     return {"message": "Welcome to Jarvic.ai"}
 
 
-def process_pdf(file):
+def process_pdf(file, custom_style):
     text = extract_text_with_style(file)
-    text = parse_data(text)
+    text = parse_data(text, custom_style)
     text = create_document(text)
     pipline_model = processing_document(text)
     return pipline_model
@@ -26,9 +26,9 @@ class PDFModelHandler:
     def __init__(self):
         pass
 
-    def process_pdf(self, pdf_file: bytes):
+    def process_pdf(self, pdf_file: bytes, custom_style: bool):
         self.pdf_file = pdf_file
-        self.model_pipline = process_pdf(pdf_file)
+        self.model_pipline = process_pdf(pdf_file, custom_style)
 
     def generate_answer(self, data):
         # Use your model to generate something
@@ -46,14 +46,14 @@ pdf_handler = PDFModelHandler()
 
 
 @app.post("/uploadpdf")
-def upload_pdf(file: UploadFile = File(...)):
+def upload_pdf(file: UploadFile = File(...), custom_style: bool = False):
     if not file.filename.endswith(".pdf"):
         return JSONResponse(status_code=400, content={"message": "Only PDF files are allowed"})
 
         # Save the uploaded file
     with open(file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    pdf_handler.process_pdf(file.filename)
+    pdf_handler.process_pdf(file.filename, custom_style)
     return JSONResponse(status_code=200, content={"message": "File uploaded successfully"})
 
 
--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,6 @@ import fitz
 
 
 def extract_text_with_style(pdf_path):
-
     doc = fitz.open(pdf_path)
     text_with_style = []
 
@@ -27,52 +26,59 @@ def extract_text_with_style(pdf_path):
 import pprint
 
 
-def parse_data(extracted_text):
+def parse_data(extracted_text, custom_style=False):
     title_text = ""
     main_title = ""
     reuslt_text = ""
     text_dict = {}
+    if not custom_style:
+        text_dict["story"] = []
     for item in extracted_text:
 
         text = item['text']
 
-        if not item['font_color'] == 0:
-            if len(reuslt_text) > 0:
+        if custom_style:
+            if not item['font_color'] == 0:
+                if len(reuslt_text) > 0:
+                    reuslt_text = main_title + " : " + title_text + " : " + reuslt_text
+
+                    if main_title in list(text_dict.keys()):
+
+                        text_dict[main_title].append(reuslt_text)
+                    else:
+                        text_dict[main_title] = []
+
+                    reuslt_text = ""
+
+                main_title = text[:-1]
+
+            elif text[0].isdigit() and text[1] == "." and len(text) > 2:
                 reuslt_text = main_title + " : " + title_text + " : " + reuslt_text
 
                 if main_title in list(text_dict.keys()):
-
                     text_dict[main_title].append(reuslt_text)
                 else:
                     text_dict[main_title] = []
 
+                title_text = text
+
                 reuslt_text = ""
 
-            main_title = text[:-1]
 
-        elif text[0].isdigit() and text[1] == "." and len(text) > 2:
-            reuslt_text = main_title + " : " + title_text + " : " + reuslt_text
+            elif text[0].isalpha() or text[0].isdigit() or text[0].startswith(" "):
+                if text.endswith(":"):
+                    reuslt_text += text + " "
+                elif text[0].isdigit() and text[1] == ".":
+                    reuslt_text += text + " "
+                else:
+                    reuslt_text += text + ", "
 
-            if main_title in list(text_dict.keys()):
-                text_dict[main_title].append(reuslt_text)
-            else:
-                text_dict[main_title] = []
-
-            title_text = text
-
-            reuslt_text = ""
+        else:
 
 
-        elif text[0].isalpha() or text[0].isdigit() or text[0].startswith(" "):
-            if text.endswith(":"):
-                reuslt_text += text + " "
-            elif text[0].isdigit() and text[1] == ".":
-                reuslt_text += text + " "
-            else:
-                reuslt_text += text + ", "
+            text_dict["story"].append(text)
+
     return text_dict
-
-
 
 # pdf_path = "/content/Copy of Foods Reciepe for farbod.pdf"
 #
